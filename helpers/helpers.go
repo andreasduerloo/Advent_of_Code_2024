d7b63a8858c7125f8557f1a4dd9700a8a60190a5
Andreas Duerloo <andreasduerloo@gmail.com>
2024-11-22 12:00:47 +0100

    Added helpers

---
+++ helpers/helpers.go
@@ -0,0 +1,46 @@
+package helpers
+
+import (
+	"errors"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func GetInput(day int) (string, error) {
+	if day > 0 && day <= 25 {
+		dayString := strconv.Itoa(day)
+
+		if len(dayString) == 1 {
+			dayString = "0" + dayString
+		}
+
+		path := "./inputs/" + dayString + ".txt"
+
+		input, err := os.ReadFile(path)
+		if err != nil {
+			return "", err
+		} else {
+			return string(input), nil
+		}
+	} else {
+		return "", errors.New("not a valid day")
+	}
+}
+
+func ReGetInts(s string) []int {
+	re := regexp.MustCompile(`-?[0-9]+`)
+	matches := re.FindAllString(s, -1)
+
+	ints := make([]int, 0)
+
+	for _, match := range matches {
+		val, err := strconv.Atoi(match)
+		if err != nil {
+			continue
+		}
+		ints = append(ints, val)
+	}
+
+	return ints
+}
