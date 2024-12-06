685abb3a758c301afd00f639fa21ccbc4f53f9f2
Andreas Duerloo <andreasduerloo@gmail.com>
2024-12-06 18:10:01 +0100

    Single threaded, but correct

--- day_06/day_06.go
+++ day_06/day_06.go
@@ -3,7 +3,6 @@ package day_06
 import (
 	"advent/helpers"
 	"fmt"
-	"sync"
 	"time"
 )
 
@@ -14,7 +13,6 @@ func Solve() (interface{}, interface{}) {
 	}
 
 	b, g := parseMap(inStr)
-	startingPosition := g.location
 
 	// First star
 	for g.inBounds {
@@ -25,18 +23,53 @@ func Solve() (interface{}, interface{}) {
 
 	// Second star
 	toTry := helpers.Uniq(g.path) // We only try points already on the path, and each point only once (even if it is on the path multiple times)
+	var second int
 
-	var validPositions int
-	found := make(chan struct{}, 5)
+	b, g = parseMap(inStr) // Load evetything back up
+	startingPosition := g.location
+
+	start := time.Now()
+
+	for _, position := range toTry {
+		if position == startingPosition {
+			continue
+		}
+
+		// Reset the guard
+		// lg.reset(startingPosition)
+		g.reset(startingPosition)
+
+		b.obstacles[position] = true
+
+		for g.inBounds && !g.cycle {
+			g.step(b)
+		}
+
+		if g.cycle {
+			second++
+		}
+
+		delete(b.obstacles, position)
+	}
+
+	fmt.Println(time.Since(start))
+	return first, second
+}
+
+/*
+var validPositions int
+	found := make(chan int, 5)
 
 	go func() {
-		for range found {
-			validPositions++
+		for result := range found {
+			validPositions += result
 		}
 	}()
 
 	// Split the input into four parts
 	parts := [][]point{toTry[0 : len(toTry)/4], toTry[(len(toTry)/4)+1 : len(toTry)/2], toTry[(len(toTry)/2)+1 : 3*len(toTry)/4], toTry[3*(len(toTry)/4)+1:]}
+
+	fmt.Println(len(toTry), len(parts[0])+len(parts[1])+len(parts[2])+len(parts[3]))
 
 	b, g = parseMap(inStr) // Load everything up from scratch
 
@@ -50,16 +83,19 @@ func Solve() (interface{}, interface{}) {
 		go func() {
 			defer wg.Done()
 
-			lg := g.copy()
+			// lg := g.copy()
 			lb := b.copy()
 
-			for _, position := range part {
-				// Reset the guard
-				lg.reset(startingPosition)
+			var localResult int
 
+			for _, position := range part {
 				if position == startingPosition {
 					continue
 				}
+
+				// Reset the guard
+				// lg.reset(startingPosition)
+				lg := g.copy()
 
 				lb.obstacles[position] = true
 
@@ -68,11 +104,13 @@ func Solve() (interface{}, interface{}) {
 				}
 
 				if lg.cycle {
-					found <- struct{}{}
+					localResult++
 				}
 
 				delete(lb.obstacles, position)
 			}
+			fmt.Println(localResult)
+			found <- localResult
 		}()
 	}
 
@@ -81,6 +119,4 @@ func Solve() (interface{}, interface{}) {
 
 	end := time.Now()
 	fmt.Println(end.Sub(start))
-
-	return first, validPositions
-}
+*/
